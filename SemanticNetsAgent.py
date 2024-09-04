2f27feb5f9e847b880da0964c109229b7624ff2e
Quinton Austin <121703507+Q-T-A@users.noreply.github.com>
2024-09-04 14:08:17 -0400

    basic

--- SemanticNetsAgent.py
+++ SemanticNetsAgent.py
@@ -19,6 +19,8 @@ class SemanticNetsAgent:
 
         queue = [(start, [])]
         visited = set()
+
+        #bfs
         while queue:
 
             cur, path = queue.pop()
@@ -30,10 +32,36 @@ class SemanticNetsAgent:
 
             moves = [(0,1), (0,2), (1,1), (1,0), (2, 0)]
             for move in moves:
-                new_state = generate_state(cur)
-                if new_state not in visited:
-                    visited.add(new_state)
-            
-        def generate_state(state):
-        pass
+                new_state = self.generate_state(sheep_left, wolves_left, boat, move)
+                if new_state not in visited and self.isValid(new_state, initial_sheep, initial_wolves):
+                    queue.append((new_state, path + [move]))
+        return []
 
+    def generate_state(self, sheep, wolf, boat, move):
+            sheep_moving, wolves_moving = move
+            if boat == 0:
+                sheep -= sheep_moving 
+                wolf -= wolves_moving
+                boat = 1
+                return (sheep, wolf, boat)
+            else: 
+                sheep += sheep_moving
+                wolf += wolves_moving
+                boat = 0
+                return (sheep, wolf, boat)
+
+
+            #return a state 
+
+    def isValid(self, state, initial_sheep,initial_wolves):
+            #return type boolean 
+            sheep_left, wolf_left, boat = state
+            sheep_right, wolf_right = initial_sheep - sheep_left, initial_wolves - wolf_left
+            #print(f'sheep_left: {sheep_left}, boat:{boat}, shee_right: {sheep_right}, \nwolf_left: {wolf_left},           wolf_right: {wolf_right}')
+            if sheep_left > initial_sheep or wolf_left > initial_wolves or sheep_left < 0 or wolf_left < 0:
+                #print('Invalid')
+                return False
+            elif sheep_left > 0 and sheep_left < wolf_left  or sheep_right  > 0 and sheep_right < wolf_right:
+                #print('Invalid')
+                return False
+            return True
