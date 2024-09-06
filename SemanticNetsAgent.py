eb1f8389cb62e0b681ea45decdb1626b3cf74a00
Quinton Austin <121703507+Q-T-A@users.noreply.github.com>
2024-09-06 11:35:08 -0400

    Added visualizer

--- SemanticNetsAgent.py
+++ SemanticNetsAgent.py
@@ -23,20 +23,26 @@ class SemanticNetsAgent:
         #bfs
         while queue:
             cur, path = queue.pop(0)
+            visited.add(cur)
+            sheep_left, wolves_left, boat = cur
+            
+            self.display_state(sheep_left, wolves_left, initial_sheep, initial_wolves, boat)
             #print(f'current path: {path}')
             if cur == end:
                 return path
             
-            visited.add(cur)
-            sheep_left, wolves_left, boat = cur
 
             moves = [(0,1), (0,2), (1,1), (1,0), (2, 0)]
+            temp = 0
             for move in moves:
                 new_state = self.generate_state(sheep_left, wolves_left, boat, move)
                 if new_state not in visited and self.isValid(new_state, initial_sheep, initial_wolves):
                     #print(f'New state: {new_state}')
+                    temp += 1
                     visited.add(new_state)
                     queue.append((new_state, path + [move]))
+            if temp == 0:
+                 print('Dead end')
         return []
 
     def generate_state(self, sheep, wolf, boat, move):
@@ -67,3 +73,26 @@ class SemanticNetsAgent:
                 #print('Invalid')
                 return False
             return True
+    
+    def display_state(self, sheep_left, wolves_left, initial_sheep, initial_wolves, boat):
+    # Sheep and wolves on the left and right banks
+        sheep_right = initial_sheep - sheep_left
+        wolves_right = initial_wolves - wolves_left
+
+        # Create the representation strings
+        s_l = f"{'S' * sheep_left}" if sheep_left > 0 else " "
+        w_l = f"{'W' * wolves_left}" if wolves_left > 0 else ' '
+        s_r = f"{'S' * sheep_right}" if sheep_right > 0 else ' '
+        w_r = f"{'W' * wolves_right}" if wolves_right > 0 else ' '
+        
+        # Show where the boat is
+        if boat == 0:
+            boat_side = "Boat______"
+        else:
+            boat_side = "______Boat"
+        filler = '          '
+        
+        # Print the current state
+        print(f"{s_l} | {boat_side} | {s_r}")
+        print(f"{w_l} | {filler} | {w_r}")
+        print("-" * 50)
--- main.py
+++ main.py
@@ -6,13 +6,13 @@ def test():
     test_agent = SemanticNetsAgent()
 
     #print(test_agent.solve(1, 1))
-    #print(test_agent.solve(2, 2))
+    print(test_agent.solve(2, 2))
     #print(test_agent.solve(3, 3))
     #print(test_agent.solve(5, 3))
     #print(test_agent.solve(6, 3))
-    print(test_agent.solve(7, 3))
-    print(test_agent.solve(5, 5))
-    print(test_agent.solve(20,10))
+    #print(test_agent.solve(7, 3))
+    #print(test_agent.solve(5, 5))
+    #print(test_agent.solve(20,10))
 
 if __name__ == "__main__":
     test()
